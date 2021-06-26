61924f2674ea82f36d72afe59789d96aa3f55f85
Kevin Wang <60906298+DefNotKevinW@users.noreply.github.com>
2021-06-26 17:38:10 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,186 @@
+/* use IFrame API */
+
+const JSONLinks = () => [
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm00.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm01.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm02.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm03.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm04.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm05.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm06.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm07.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm08.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm09.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm10.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm11.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm12.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm13.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm14.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm15.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm16.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm17.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm18.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm19.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm20.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm21.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm22.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm23.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm24.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm25.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm26.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm27.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm28.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm29.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm30.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm31.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm32.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm33.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm34.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm35.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm36.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm37.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm38.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm39.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm40.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm41.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm42.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm43.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm44.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm45.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm46.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm47.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm48.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm49.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm50.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm51.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm52.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm53.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm54.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm55.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm56.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm57.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmAoT.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmBR.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmBT.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmCN.json",
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmEvent.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmEvent2.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmFT.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmGL.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmGL2D.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmJp.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmJp2.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmJp3.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmJp4_Zipang.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmMH.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmMIKU.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmMY.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmMultiTrack.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmPL.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmPL2.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmSG.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmSG_HeadPhone.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmStarPlanet_JP.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmTH.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmTW.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmUI.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmWz2.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/BgmWz2Chair.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/Bgm_pachinko.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/GL_Masteria.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/PL_Beautyroid.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/PL_CaptainNomad.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/PL_MMF.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/PL_WinterFrenzy.json", 
+        "https://defnotkevinw.github.io/Maplestory-BGM-JSON/bgm/PL_sengoku3.json"
+];
+
+var naviVar = {
+    pages: [], /* page number starts from 0 */
+    currPage: 0, /* start on first page */
+    nPP: 25 /* number of BGMs displayed per page */
+}
+
+var BGMList = {
+    list: [],
+    filtered: []
+}
+
+Promise.all(JSONLinks().map(link => fetch(link)))
+    .then(promises => Promise.all(promises.map(response => response.json())))
+    .then(data => {
+        let BGMData = [];
+        data.forEach(arr => BGMData = BGMData.concat(arr));
+
+        BGMList.list = BGMData;
+        BGMList.filtered = BGMData;
+        createPages();
+        showResults();
+
+        document.querySelector(".left").addEventListener("click", () => {
+            if (naviVar.currPage > 0) {
+                showResults(naviVar.currPage - 1);
+                document.querySelector(".currentPage").innerHTML = naviVar.currPage + 1;
+            }
+        });
+
+        document.querySelector(".right").addEventListener("click", () => {
+            if (naviVar.currPage < naviVar.pages.length - 1) {
+                showResults(naviVar.currPage + 1);
+                document.querySelector(".currentPage").innerHTML = naviVar.currPage + 1;
+            }
+        });
+
+        document.querySelector(".searchBar").addEventListener("keyup", (e) => {
+            const input = e.target.value.toLowerCase();
+            
+            applySearchFilter(input);
+            createPages();
+            showResults();
+        });
+    });
+
+const createPages = () => {
+    /* creates the needed number of pages for the filtered BGMs. */
+    let nPages = Math.ceil(BGMList.filtered.length / 25);
+
+    naviVar.pages = [...Array(nPages).keys()];
+}
+
+const applySearchFilter = (input) => {
+    /* filters the filtered list according to whats typed in the search bar. */
+    BGMList.filtered = BGMList.list.filter(bgm => 
+        bgm.mark.toLowerCase().includes(input) || bgm.metadata.title.toLowerCase().includes(input));
+}
+
+const showResults = (newPage = 0) => {
+    clearResults();
+
+    /* set the current page to newPage */
+    naviVar.currPage = newPage;
+    document.querySelector(".currentPage").innerHTML = naviVar.currPage + 1;
+
+    addBGMs();
+}
+
+const addBGMs = () => {
+    /* shows the bgms on the DOM from current page. */
+    let start = naviVar.currPage * naviVar.nPP;
+    let end = start + naviVar.nPP;
+    BGMList.filtered.slice(start, end).map(bgm => {
+        createBGM(bgm);
+    });
+}
+
+/* clears the results shown on the DOM. */
+const clearResults = () => document.querySelector(".results").innerHTML = "";
+
+const createBGM = (bgm) => {
+    /* creates the DOM object for the bgm. */
+    let li;
+
+    li = document.createElement("li");
+    li.appendChild(document.createTextNode(bgm.metadata.title));
+
+    document.querySelector(".results").appendChild(li);
+}
